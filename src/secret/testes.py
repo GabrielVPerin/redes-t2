2d4304ba6b184f2381ba41454ee541d50b1eabd6
Gabriel Vitor Perin <gvp24@inf.ufpr.br>
2026-06-16 14:50:07 -0300

    consertado erro de send

--- src/secret/testes.py
+++ src/secret/testes.py
@@ -12,7 +12,7 @@ MAC_VETH8 = "00:00:00:00:00:02"
 pkt_update = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0xFFFF) / TokenHeader(token=0xABCD)
 
 print("Atualizado token secreto")
-send(pkt_update, iface="veth0")
+sendp(pkt_update, iface="veth0")
 
 pkt_normal_certo = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0x1111) / TokenHeader(token=0xABCD)
 
