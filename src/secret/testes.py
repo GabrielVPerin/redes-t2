a18759dd417079c2f16706ca49ae65f5bbe44fec
Gabriel Perin <gvp24@inf.ufpr.br>
2026-06-17 10:55:39 -0300

    adicionado mais testes

--- src/secret/testes.py
+++ src/secret/testes.py
@@ -13,17 +13,26 @@ TOKEN1 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 TOKEN2 = 0x99999999999999999999999999999999
 TOKEN3 = 0x11111111111111111111111111111111
 
-pkt_update = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0xFFFF) / TokenHeader(token=TOKEN1)
-
+pacote = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0xFFFF) / TokenHeader(token=TOKEN1)
 print("Atualizado token secreto")
-sendp(pkt_update, iface="veth0")
+sendp(pacote, iface="veth0")
 
-pkt_normal_certo = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0x1111) / TokenHeader(token=TOKEN1)
-
+pacote = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0x1111) / TokenHeader(token=TOKEN1)
 print("Enviado pacote com token correto")
-sendp(pkt_normal_certo, iface="veth0")
+sendp(pacote, iface="veth0")
 
-pkt_normal_errado = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0x1111) / TokenHeader(token=TOKEN2)
-
+pacote = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0x1111) / TokenHeader(token=TOKEN2)
 print("Enviado pacote com token errado")
-sendp(pkt_normal_errado, iface="veth0")
+sendp(pacote, iface="veth0")
+
+pacote = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0xFFFF) / TokenHeader(token=TOKEN3)
+print("Atualizado token secreto")
+sendp(pacote, iface="veth0")
+
+pacote = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0x1111) / TokenHeader(token=TOKEN1)
+print("Enviado pacote com token antigo")
+sendp(pacote, iface="veth0")
+
+pacote = Ether(src=MAC_VETH0, dst=MAC_VETH8, type=0x1111) / TokenHeader(token=TOKEN3)
+print("Enviado pacote com token novo")
+sendp(pacote, iface="veth0")
